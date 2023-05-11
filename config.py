8e9fe51db04579573b5bc5ec41025b3b76e29420
ICC TEAM <131507368+GOVIND-BOTS@users.noreply.github.com>
2023-05-11 23:00:07 +0530

    config.py

--- config.py
+++ config.py
@@ -5,23 +5,23 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID", "13976276"))
+API_HASH = getenv("API_HASH", "7f024cbc744a2f44569c3641b5ccecb7")
 
-BOT_TOKEN = getenv("BOT_TOKEN", None)
+BOT_TOKEN = getenv("BOT_TOKEN", "6052195748:AAFpuknQjZ9OOnqoTKoL2515BhhneWW_ZRI")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
-OWNER_ID = int(getenv("OWNER_ID"))
+OWNER_ID = int(getenv("OWNER_ID", "6008136265"))
 
 PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
+START_IMG = getenv("START_IMG", "https://te.legra.ph/file/b2258dbee2401c426eb3c.jpg")
 
-SESSION = getenv("SESSION", None)
+SESSION = getenv("SESSION", "BQCt7yLuHRP5IXO66b7My80q4-cd1uIJxgNTU1R3391WSIULfnXWJOvJxREGEQa-xaYkUAM4mkKdCN35DqljR_qsAF-OkjB3xwSWRteM9Iqr4_E4f9AeXHAayO3tyYIOd7nvLdFLhiveqONQZfjDTUpgCX0U2tB-_8eplWja5WUKJAFQyzANivHrIods_t74ZgNar_dNNZr9OIKLMX-Qwi1FChXzeFL6At6DM0-9ZrEcdhJUOF_tqgDgPaoqBKw38oxD9IcCmqqbSUzeteGnjxE3KueUUuTsUm1pxKS0wg83ytXOj9GCo_LzNSszROECIJTYO1pHdJm55KmyQOkfN5HsAAAAAUgLerAA") 
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/indian_chatting_club_offical")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "http://t.me/tha_govind_op")
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6020570673").split()))
 
 
 FAILED = "https://te.legra.ph/file/4c896584b592593c00aa8.jpg"
